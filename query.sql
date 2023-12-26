3d717415fc8128168afe4855fa81c4e0943e96bb
PeschykhinLev <91117670+PeschykhinLev@users.noreply.github.com>
2023-12-26 16:37:31 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,13 +4,14 @@ FROM Meal
 JOIN Nutritional_Information ON Meal.item_id = Nutritional_Information.item_id
 ORDER BY protein
 
--- Категорія страви, які мають від 100 калорій та кількість цих калорій
-SELECT Category.category_name, Nutritional_Information.Calories
+-- Загальна кількість калорій у кожній категорії
+SELECT Category.category_name, SUM(Nutritional_Information.Calories) AS Total_Calories
 FROM Category
 JOIN Meal_Category ON Category.category_id = Meal_Category.category_id
 JOIN Nutritional_Information ON Meal_Category.item_id = Nutritional_Information.item_id
 WHERE Nutritional_Information.Calories > 100
-ORDER BY Calories;
+GROUP BY Category.category_name
+ORDER BY Total_Calories;
 
 -- Назва страви, та впорядковане порівняння (за зростаннія) її кількості калорій та % від денної форми жирів
 SELECT Meal.item_name, Nutritional_Information.Calories, Daily_Values_on_the_Nutrition.Total_Fat_Daily_Percentage
